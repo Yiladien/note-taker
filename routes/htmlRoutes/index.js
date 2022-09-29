776927613b340b187d74e1e2198f08448b8886c3
yiladien@hotmail.com <yiladien@hotmail.com>
2022-09-29 07:32:25 -0500

    routing path url

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,20 +1,16 @@
-const path = require('path');
-const router = require('express').Router();
+const path = require("path");
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
-router.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/animals.html'));
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
 
-router.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
-});
-
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 module.exports = router;
